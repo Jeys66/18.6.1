04894a795d7b73920207ddd374ae5a37c1d78454
Jeys66 <105886658+Jeys66@users.noreply.github.com>
2022-08-15 20:37:46 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = '5442064853:AAHeC4WNSiUZVul3ZZ17RNvLrEFLqv7slt4'
+
+keys ={
+    'bitcoin': 'BTC',
+    'ethereum': 'ETH',
+    'solana': 'SOL',
+    'tether': 'USDT',
+    'xrp': 'XRP',
+    'usd': 'USD',
+    'jpy': 'JPY',
+    'eur': 'EUR',
+    'руб': 'RUB',
+    'bat': 'BAT',
+    'юань': 'CNY',
+}
